3f883e23e19889c4855a149cfd922ee26dacfc09
Razican <razican@protonmail.ch>
2016-01-22 15:40:13 +0100

    This should fix the build

--- prepare-script.sh
+++ prepare-script.sh
@@ -5,9 +5,9 @@ echo "[target.arm-unknown-linux-gnueabihf]" >> .cargo/config
 echo "ar = \"arm-linux-gnueabihf-gcc-ar\"" >> .cargo/config
 echo "linker = \"arm-linux-gnueabihf-gcc\"" >> .cargo/config
 
-dir=rust-std-$TARGET
-pkg=rust-std
+dir=rust-std-arm-unknown-linux-gnueabihf
+tarball=https://static.rust-lang.org/dist/rust-std-$TRAVIS_RUST_VERSION-arm-unknown-linux-gnueabihf.tar.gz
 
-curl -s $MAIN_TARGETS/$pkg-$TRAVIS_RUST_VERSION-$TARGET.tar.gz | \
+curl -s $tarball | \
 tar xzf - -C $HOME/rust/lib/rustlib --strip-components=4 \
-  $pkg-$TRAVIS_RUST_VERSION-$TARGET/$dir/lib/rustlib/$TARGET
+  rust-std-$TRAVIS_RUST_VERSION-arm-unknown-linux-gnueabihf/$dir/lib/rustlib/arm-unknown-linux-gnueabihf
