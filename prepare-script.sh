2f751bef052e8407dd85731be184ee02be73a75c
Razican <razican@protonmail.ch>
2016-01-22 13:06:09 +0100

    Removed versions prior to Rust 1.5.0

--- prepare-script.sh
+++ prepare-script.sh
@@ -3,10 +3,6 @@ set -ex
 dir=rust-std-$TARGET
 pkg=rust-std
 
-if [ "$TRAVIS_RUST_VERSION" = "1.0.0" ]; then
-    pkg=rust
-    dir=rustc
-fi
 curl -s $MAIN_TARGETS/$pkg-$TRAVIS_RUST_VERSION-$TARGET.tar.gz | \
 tar xzf - -C $HOME/rust/lib/rustlib --strip-components=4 \
   $pkg-$TRAVIS_RUST_VERSION-$TARGET/$dir/lib/rustlib/$TARGET
